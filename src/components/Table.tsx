565b6739b382adae32f77a5607f96ef38206c4c9
YTim <yun2193@hanmail.net>
2023-03-21 23:02:33 +0900

    달력 animation 부여

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,15 +1,42 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { motion, Variants, AnimatePresence } from "framer-motion";
 
 const Calendar = styled.div`
-  width: 50%;
+  width: 500px;
 `;
 
-const Name = styled.h4`
+const Head = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px;
+  position: relative;
+`;
+
+const Name = styled(motion.h4)`
   font-size: 45px;
   font-weight: 600;
-  margin-bottom: 20px;
   color: rgba(255, 255, 255, 0.8);
+  position: absolute;
+`;
+
+const Btns = styled.div`
+  position: absolute;
+  right: 0;
+`;
+
+const Btn = styled.button`
+  background-color: rgba(255, 255, 255, 0.8);
+  border: none;
+  color: rgba(0, 0, 0, 0.7);
+  padding: 5px;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+  &:last-child {
+    margin-left: 10px;
+  }
 `;
 
 const Month = styled.div`
@@ -32,13 +59,13 @@ const DayOfName = styled.span`
   color: rgba(255, 255, 255, 0.8);
 `;
 
-const Week = styled.div`
+const Week = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   gap: 5px;
 `;
 
-const Day = styled.div`
+const Day = styled(motion.div)`
   background-color: aliceblue;
   border-radius: 7px;
   display: flex;
@@ -48,44 +75,126 @@ const Day = styled.div`
   height: 70px;
 `;
 
+const nameVariant: Variants = {
+  start: (direction: number) => {
+    return {
+      y: direction > 0 ? 50 : -50,
+      opacity: 0,
+    };
+  },
+  move: {
+    y: 0,
+    opacity: 1,
+  },
+  end: (direction: number) => {
+    return {
+      y: direction < 0 ? 50 : -50,
+      opacity: 0,
+    };
+  },
+};
+
+const weekVariant: Variants = {
+  initial: (direction: number) => {
+    return {
+      x: direction > 0 ? 300 : -300,
+      opacity: 0,
+    };
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return { x: direction < 0 ? 300 : -300, opacity: 0 };
+  },
+};
+
 function Table() {
   const [year, setYear] = useState<number>(2023);
-  const [month, setMonth] = useState<number>(1);
+  const [[month, engMonth], setMonth] = useState<[number, string]>([1, "Jan"]);
   const [date, setDate] = useState<number[]>([]);
+  const [direction, setDirection] = useState<number>(0);
   useEffect(() => {
-    setYear(new Date().getFullYear());
-    setMonth(new Date().getMonth() + 1);
+    const today = new Date();
+    setYear(today.getFullYear());
+    setMonth([today.getMonth() + 1, today.toString().slice(4, 7)]);
   }, []);
   useEffect(() => {
     let dates = [];
     const lastDay = new Date(year, month, 0);
     const dayOfFirst = new Date(year, month - 1).getDay();
-    console.log("dayOfFirst", dayOfFirst);
-    for (let i = 0; i < dayOfFirst; i++) {
-      dates.push(0);
+    for (let i = 0, j = 0; i < dayOfFirst; i++, j--) {
+      dates.push(j);
     }
     for (let i = 1; i <= lastDay.getDate(); i++) {
       dates.push(i);
     }
     setDate(dates);
   }, [month, year]);
+  const goPreviousMonth = (newDirection: number) => {
+    const today = new Date(`${year}-${month - 1}`);
+    setMonth((prev) =>
+      prev[0] === 1 ? [12, "Dec"] : [prev[0] - 1, today.toString().slice(4, 7)]
+    );
+    setYear((prev) => (month === 1 ? prev - 1 : prev));
+    setDirection(newDirection);
+  };
+  const goNextMonth = (newDirection: number) => {
+    const today = new Date(`${year}-${month + 1}`);
+    setMonth((prev) =>
+      prev[0] === 12 ? [1, "Jan"] : [prev[0] + 1, today.toString().slice(4, 7)]
+    );
+    setYear((prev) => (month === 12 ? prev + 1 : prev));
+    setDirection(newDirection);
+  };
   return (
     <Calendar>
-      <Name>3 March</Name>
+      <Head>
+        <AnimatePresence initial={false} custom={direction}>
+          <Name
+            key={year + "1" + month}
+            custom={direction}
+            variants={nameVariant}
+            initial="start"
+            animate="move"
+            exit="end"
+          >
+            {month} {engMonth} {year}
+          </Name>
+        </AnimatePresence>
+        <Btns>
+          <Btn onClick={() => goPreviousMonth(-1)}>{"◀"}</Btn>
+          <Btn onClick={() => goNextMonth(1)}>{"▶"}</Btn>
+        </Btns>
+      </Head>
       <Month>
         <DayOfNames>
           {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((item) => (
             <DayOfName key={item}>{item}</DayOfName>
           ))}
         </DayOfNames>
-        <Week>
-          {date.map((item) =>
-            item === 0 ? (
-              <Day style={{ visibility: "hidden" }}></Day>
-            ) : (
-              <Day key={item}>{item}</Day>
-            )
-          )}
+        <Week
+          key={year + "2" + month}
+          custom={direction}
+          variants={weekVariant}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+        >
+          <AnimatePresence initial={false} custom={direction}>
+            {date.map((item) =>
+              item <= 0 ? (
+                <Day key={item} style={{ visibility: "hidden" }}></Day>
+              ) : (
+                <Day key={item}>{item}</Day>
+              )
+            )}
+          </AnimatePresence>
         </Week>
       </Month>
     </Calendar>
