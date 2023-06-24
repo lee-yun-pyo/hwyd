e44a71a18e811acf260ea4f852afa22c5083404b
YTim <yun2193@hanmail.net>
2023-06-24 21:46:47 +0900

    style: WrapperDay hover 스타일 추가

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -102,7 +102,9 @@ const WrapperDay = styled(motion.div)<IDay>`
   font-size: 24px;
   color: ${(props) =>
     props.istoday || props.scoreday ? "rgba(255,255,255,0.8)" : "#000"};
+  transition: all 0.1s ease-in-out;
   &:hover {
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
   }
 `;
 
